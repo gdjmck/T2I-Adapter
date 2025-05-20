6019ca69cb69b315e0bc256feb3f3639ede1a52c
chenkang <lv23ck@hotmail.com>
2025-05-20 11:32:44 +0800

    modify: test.py

--- test.py
+++ test.py
@@ -1,37 +1,37 @@
 from diffusers import StableDiffusionXLAdapterPipeline, T2IAdapter, EulerAncestralDiscreteScheduler, AutoencoderKL
 from diffusers.utils import load_image, make_image_grid
-from controlnet_aux.lineart import LineartDetector
+from omegaconf import OmegaConf
+from configs.utils import instantiate_from_config
 import torch
 
 if __name__ == '__main__':
+    # configs
+    config = OmegaConf.load('configs/train/Adapter-XL-layout.yaml')
     # load adapter
-    adapter = T2IAdapter.from_pretrained(
-      "TencentARC/t2i-adapter-lineart-sdxl-1.0", torch_dtype=torch.float16, varient="fp16"
-    ).to("cuda")
+    adapter_config = config.model.params.adapter_config
+    adapter = instantiate_from_config(adapter_config).cuda()
+    adapter.load_state_dict(torch.load('experiments/adapter_sketch_xl/checkpoint-9000/model-00.pth'))
     
     # load euler_a scheduler
     model_id = 'stabilityai/stable-diffusion-xl-base-1.0'
     euler_a = EulerAncestralDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
-    vae=AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16)
+    vae = AutoencoderKL.from_pretrained(
+        model_id,
+        subfolder=None,
+    )
     pipe = StableDiffusionXLAdapterPipeline.from_pretrained(
         model_id, vae=vae, adapter=adapter, scheduler=euler_a, torch_dtype=torch.float16, variant="fp16", 
     ).to("cuda")
     pipe.enable_xformers_memory_efficient_attention()
-    
-    line_detector = LineartDetector.from_pretrained("lllyasviel/Annotators").to("cuda")
-    
-    url = "https://huggingface.co/Adapter/t2iadapter/resolve/main/figs_SDXLV1.0/org_lin.jpg"
-    image = load_image(url)
-    image = line_detector(
-        image, detect_resolution=384, image_resolution=1024
-    )
-    
-    prompt = "Ice dragon roar, 4k photo"
-    negative_prompt = "anime, cartoon, graphic, text, painting, crayon, graphite, abstract, glitch, deformed, mutated, ugly, disfigured"
+
+    control_image = load_image("./conditioning_image_1.png")
+    prompt = "A layout plan of residential community, with a total of 3 buildings, a floor area ratio of 2.50, a building density of 30.8%, and an average number of floors of 6.7"
+
+    negative_prompt = "colorful"
     gen_images = pipe(
         prompt=prompt,
         negative_prompt=negative_prompt,
-        image=image,
+        image=control_image,
         num_inference_steps=30,
         adapter_conditioning_scale=0.8,
         guidance_scale=7.5, 
